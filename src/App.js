c42ff29524c40e91ac18c5ae1482d3adca4e3774
TuanAnhNguyen <tuananh1210085213@gmail.com>
2022-07-21 10:34:07 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import { BrowserRouter, Route, Switch, Redirect, Link } from "react-router-dom";
+import Header from "./components/Header";
+import NotFound from "./components/NotFound";
+
+// Lazy load - Code splitting
+const Photo = React.lazy(() => import("./features/Photo"));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="photo-app">
+      <Suspense fallback={<div>Loading ...</div>}>
+        <BrowserRouter>
+          <Header />
+
+          <Switch>
+            <Redirect exact from="/" to="/photos" />
+
+            <Route path="/photos" component={Photo} />
+            <Route component={NotFound} />
+          </Switch>
+        </BrowserRouter>
+      </Suspense>
     </div>
   );
 }
---
+++ src/constants/images.js
@@ -0,0 +1,13 @@
+import orangeBackground from "../assets/images/orange-bg.jpg";
+import blueBackground from "../assets/images/WorkHard.jpg";
+import colorfulBackground from "../assets/images/DreamBig.jpg";
+import pinkBackground from "../assets/images/pink-bg.jpg";
+
+const Images = {
+  ORANGE_BG: orangeBackground,
+  BLUE_BG: blueBackground,
+  COLORFUL_BG: colorfulBackground,
+  PINK_BG: pinkBackground,
+};
+
+export default Images;
